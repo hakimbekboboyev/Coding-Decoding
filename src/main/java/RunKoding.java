8e178c5cf3457755b33131e02399f75f5a62b107
hakimbekboboyev <87408338+hakimbekboboyev@users.noreply.github.com>
2023-05-05 19:36:56 +0500

    Coding-Decoding

---
+++ src/main/java/RunKoding.java
@@ -0,0 +1,9 @@
+public class RunKoding {
+    public static void main(String[] args) {
+        KodingForm form = new KodingForm();
+
+
+        form.setVisible(true);
+        form.setLocationRelativeTo(null);
+    }
+}
